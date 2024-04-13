cef177e16707a5df8be114abfd37a3a8ee786a27
Igor Leskov <151756679+igor-leskov@users.noreply.github.com>
2024-04-13 12:22:02 +0300

    Update like.js

--- scripts/like.js
+++ scripts/like.js
@@ -1,22 +1,16 @@
 const newsFeed = document.querySelector('.news-feed');
 
-// Обработчик клика по кнопкам лайка внутри родительского элемента новостей
 newsFeed.addEventListener('click', function(event) {
-    // Получаем элемент, на который был сделан клик
     const target = event.target;
 
-    // Проверяем, является ли элемент кнопкой "like" или находится ли он внутри такой кнопки
     if (target.classList.contains('like-button') || target.closest('.like-button')) {
         // Если да, получаем саму кнопку "like"
         const button = target.closest('.like-button');
-        // Вызываем функцию обновления счетчика лайков
         updateLikeCount(button);
-        // Вызываем функцию сохранения лайков в куки
         saveLikesToCookie(button);
     }
 });
 
-// Функция для обновления счетчика лайков
 function updateLikeCount(button) {
     let count = parseInt(button.dataset.likes) || 0;
     count++;
@@ -29,7 +23,6 @@ function updateLikeCount(button) {
     document.cookie = `likes_${buttonId}=${count}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
 }
 
-// При загрузке страницы восстанавливаем счетчики лайков из куки
 window.onload = function() {
     const likeButtons = document.querySelectorAll('.like-button');
     likeButtons.forEach(button => {
@@ -42,7 +35,6 @@ window.onload = function() {
     });
 };
 
-// Функция для получения значения куки по имени
 function getCookie(name) {
     const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
@@ -54,7 +46,6 @@ function getCookie(name) {
     return null;
 }
 
-// Функция для сохранения информации о лайках в куки
 function saveLikesToCookie(button) {
     const buttonId = button.dataset.id;
     const count = button.dataset.likes || 0;
