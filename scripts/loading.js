f0de8f732923f93261f679305404a0c97dafc3c9
Igor Leskov <151756679+igor-leskov@users.noreply.github.com>
2024-04-13 11:44:17 +0300

    Update loading.js

--- scripts/loading.js
+++ scripts/loading.js
@@ -17,14 +17,10 @@ def lazy_load_images(image_urls):
         images.append(load_image_lazy(image_url))
     return images
 
-# Пример использования:
-image_urls = [...]  # список URL-адресов изображений для загрузки
+image_urls = [...]  
 
-# Стремительная загрузка
 eager_loaded_images = eager_load_images(image_urls)
 
-# Автоматическая загрузка
 auto_loaded_images = auto_load_images(image_urls)
 
-# Отложенная загрузка
 lazy_loaded_images = lazy_load_images(image_urls)
