79e78b136f38cc43a62dc35fb007588e832f3c1f
Igor Leskov <151756679+igor-leskov@users.noreply.github.com>
2024-04-14 11:52:37 +0300

    Update cookie.js

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -75,6 +75,72 @@ function updateTotalLikes() {
   });
 }
 
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function saveLikesToCookie(button) {
+    const consentAccepted = getCookie("cookieconsent"); 
+    if (consentAccepted) { 
+        const buttonId = button.dataset.id;
+        const count = button.dataset.likes || 0;
+        document.cookie = `likes_${buttonId}=${count}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+    }
+}
+
+function acceptCookies() {
+    setCookie("cookieconsent", "accepted", 30);
+    var consentBar = document.getElementById("cookie-consent-bar");
+    if (consentBar) {
+        consentBar.style.display = "none"; 
+    }
+}
+
+function fixCookieConsentBarPosition() {
+    var consentBar = document.getElementById("cookie-consent-bar");
+    if (consentBar) {
+        consentBar.style.position = "fixed";
+    }
+}
+
+function redirectToPrivacyPolicyRu() {
+    var policyPage = "privacy_policy.html";
+    window.location.href = policyPage;
+}
+
+function redirectToPrivacyPolicyEt() {
+    var policyPage = "privacy_policy_et.html"; 
+    window.location.href = policyPage;
+}
+
+window.addEventListener('load', function() {
+    
+    if (!getCookie("cookieconsent")) {
+        var consentBar = document.getElementById("cookie-consent-bar");
+        if (consentBar) {
+            consentBar.style.display = "block";
+        }
+    }
+
+    fixCookieConsentBarPosition();
+
+    const likeButtons = document.querySelectorAll('.like-button');
+    likeButtons.forEach(button => {
+        const buttonId = button.dataset.id;
+        const likesCookie = getCookie(`likes_${buttonId}`);
+        if (likesCookie) {
+            button.dataset.likes = likesCookie;
+            button.querySelector('.like-count').textContent = likesCookie;
+        }
+    });
+});
+
 // Обработчик события для кнопки лайка
 document.addEventListener('DOMContentLoaded', function() {
   const likeButtons = document.querySelectorAll('.like-button');
