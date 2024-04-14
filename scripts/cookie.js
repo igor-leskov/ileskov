0ccde44a15059ff7e28a558fa796cbefd0c3ae02
Igor Leskov <151756679+igor-leskov@users.noreply.github.com>
2024-04-14 11:17:12 +0300

    Update cookie.js

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -1,40 +1,83 @@
-// Обработчик клика на кнопку лайка
+const newsFeed = document.querySelector('.news-feed');
+
+if (newsFeed) {
+    newsFeed.addEventListener('click', function(event) {
+        const target = event.target;
+        if (target.classList.contains('like-button') || target.closest('.like-button')) {
+            const button = target.closest('.like-button');
+            if (!button.dataset.likes) {
+                updateLikeCount(button);
+                saveLikesToCookie(button);
+            }
+        }
+    });
+}
+
 function updateLikeCount(button) {
     let count = parseInt(button.dataset.likes) || 0;
     count++;
     button.dataset.likes = count;
     const likeCount = button.querySelector('.like-count');
     likeCount.textContent = count;
-
-    // Обновление общего количества лайков в localStorage
-    updateTotalLikes(1); // Увеличиваем на 1 при каждом новом лайке
-
-    // Обновление показателя лайков для этой новости
-    const newsItem = button.closest('.news-item');
-    const newsLikesCount = newsItem.querySelector('.total-likes-news');
-    newsLikesCount.textContent = count;
 }
 
-// Добавляем обработчики событий
-window.addEventListener('load', function() {
-    // Обновляем общее количество лайков при загрузке страницы
-    updateTotalLikesOnLoad();
-
-    const newsFeed = document.querySelector('.news-feed');
-    if (newsFeed) {
-        newsFeed.addEventListener('click', function(event) {
-            const target = event.target;
-            if (target.classList.contains('like-button') || target.closest('.like-button')) {
-                const button = target.closest('.like-button');
-                if (!button.dataset.likes) {
-                    updateLikeCount(button);
-                    saveLikesToLocalStorage(button);
-                }
-            }
-        });
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
     }
+    return null;
+}
 
-    // Показать панель cookie, если пользователь еще не согласился
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function saveLikesToCookie(button) {
+    const consentAccepted = getCookie("cookieconsent"); 
+    if (consentAccepted) { 
+        const buttonId = button.dataset.id;
+        const count = button.dataset.likes || 0;
+        document.cookie = `likes_${buttonId}=${count}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+    }
+}
+
+function acceptCookies() {
+    setCookie("cookieconsent", "accepted", 30);
+    var consentBar = document.getElementById("cookie-consent-bar");
+    if (consentBar) {
+        consentBar.style.display = "none"; 
+    }
+}
+
+function fixCookieConsentBarPosition() {
+    var consentBar = document.getElementById("cookie-consent-bar");
+    if (consentBar) {
+        consentBar.style.position = "fixed";
+    }
+}
+
+function redirectToPrivacyPolicyRu() {
+    var policyPage = "privacy_policy.html";
+    window.location.href = policyPage;
+}
+
+function redirectToPrivacyPolicyEt() {
+    var policyPage = "privacy_policy_et.html"; 
+    window.location.href = policyPage;
+}
+
+window.addEventListener('load', function() {
+    
     if (!getCookie("cookieconsent")) {
         var consentBar = document.getElementById("cookie-consent-bar");
         if (consentBar) {
@@ -44,37 +87,14 @@ window.addEventListener('load', function() {
 
     fixCookieConsentBarPosition();
 
-    // Обработчик клика на кнопку согласия с использованием файлов cookie
-    const acceptButton = document.getElementById("accept-cookies-button");
-    if (acceptButton) {
-        acceptButton.addEventListener("click", function() {
-            // Сохраняем согласие пользователя в cookie
-            acceptCookies();
-            // Скрываем панель cookie
-            var consentBar = document.getElementById("cookie-consent-bar");
-            if (consentBar) {
-                consentBar.style.display = "none";
-            }
-        });
-    }
-
     const likeButtons = document.querySelectorAll('.like-button');
     likeButtons.forEach(button => {
         const buttonId = button.dataset.id;
-        const likesLocalStorage = localStorage.getItem(`likes_${buttonId}`);
-        if (likesLocalStorage) {
-            button.dataset.likes = likesLocalStorage;
-            button.querySelector('.like-count').textContent = likesLocalStorage;
-            // Устанавливаем показатель лайков для каждой новости
-            const newsItem = button.closest('.news-item');
-            const newsLikesCount = newsItem.querySelector('.total-likes-news');
-            newsLikesCount.textContent = likesLocalStorage;
+        const likesCookie = getCookie(`likes_${buttonId}`);
+        if (likesCookie) {
+            button.dataset.likes = likesCookie;
+            button.querySelector('.like-count').textContent = likesCookie;
         }
     });
-
-    // Обновление общего количества лайков при загрузке страницы
-    const totalLikes = parseInt(localStorage.getItem('totalLikes')) || 0;
-    // Показать общее количество лайков на странице
-    document.querySelector('.total-likes').textContent = totalLikes;
 });
 
