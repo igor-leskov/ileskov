93861c6c64d8c1d95912a076c845af643f048f5f
Igor Leskov <151756679+igor-leskov@users.noreply.github.com>
2024-04-16 08:26:41 +0300

    Update mob_audio_player.js

--- scripts/mob_audio_player.js
+++ scripts/mob_audio_player.js
@@ -1,53 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() {
-        var musicItems = document.querySelectorAll(".music-item");
+    var currentAudioPlayer = null;
+    var musicItems = document.querySelectorAll(".music-item");
 
-        musicItems.forEach(function(item, index) {
-            var playButton = item.querySelector(".play-button");
-            var audioSrc = playButton.dataset.src;
-            var audioPlayer = new Audio(audioSrc);
-            var seekBar = item.querySelector(".seek-bar");
+    musicItems.forEach(function(item, index) {
+        var playButton = item.querySelector(".play-button");
+        var audioSrc = playButton.dataset.src;
+        var audioPlayer = new Audio(audioSrc);
+        var seekBar = item.querySelector(".seek-bar");
+        var playIcon = playButton.querySelector(".play-icon");
+        var pauseIcon = playButton.querySelector(".pause-icon");
 
-            playButton.addEventListener("click", function() {
-                
-                if (currentAudioPlayer && currentAudioPlayer !== audioPlayer) {
-                    currentAudioPlayer.pause(); 
-                    var playButtonPrev = document.querySelector(".playing");
-                    if (playButtonPrev) {
-                        playButtonPrev.innerHTML = "&#9658;"; 
-                        playButtonPrev.classList.remove("playing"); 
-                    }
+        playButton.addEventListener("click", function() {
+            if (currentAudioPlayer && currentAudioPlayer !== audioPlayer) {
+                currentAudioPlayer.pause();
+                var playButtonPrev = document.querySelector(".playing");
+                if (playButtonPrev) {
+                    playButtonPrev.querySelector(".pause-icon").style.display = 'none';
+                    playButtonPrev.querySelector(".play-icon").style.display = 'block';
+                    playButtonPrev.classList.remove("playing");
                 }
-                
-                currentAudioPlayer = audioPlayer; 
-                if (audioPlayer.paused) {
-                    audioPlayer.play();
-                    playButton.innerHTML = "&#10074;&#10074;"; 
-                    playButton.classList.add("playing"); // 
-                } else {
-                    audioPlayer.pause();
-                    playButton.innerHTML = "&#9658;"; 
-                    playButton.classList.remove("playing"); 
-                }
-            });
+            }
 
-            
-            audioPlayer.addEventListener("ended", function() {
-                playButton.innerHTML = "&#9658;"; 
-                seekBar.value = 0;
-                if (index < musicItems.length - 1) {
-                    var nextButton = musicItems[index + 1].querySelector(".play-button");
-                    nextButton.click();
-                }
-            });
+            currentAudioPlayer = audioPlayer;
+            if (audioPlayer.paused) {
+                audioPlayer.play();
+                playIcon.style.display = 'none';
+                pauseIcon.style.display = 'block';
+                playButton.classList.add("playing");
+            } else {
+                audioPlayer.pause();
+                playIcon.style.display = 'block';
+                pauseIcon.style.display = 'none';
+                playButton.classList.remove("playing");
+            }
+        });
 
-            
-            audioPlayer.addEventListener("timeupdate", function() {
-                seekBar.value = audioPlayer.currentTime;
-            });
+        audioPlayer.addEventListener("ended", function() {
+            playIcon.style.display = 'block';
+            pauseIcon.style.display = 'none';
+            seekBar.value = 0;
+            if (index < musicItems.length - 1) {
+                var nextButton = musicItems[index + 1].querySelector(".play-button");
+                nextButton.click();
+            }
+        });
 
-            
-            seekBar.addEventListener("change", function() {
-                audioPlayer.currentTime = seekBar.value;
-            });
+        audioPlayer.addEventListener("timeupdate", function() {
+            if (!isNaN(audioPlayer.duration)) {
+                seekBar.value = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+            }
+        });
+
+        seekBar.addEventListener("input", function() {
+            audioPlayer.currentTime = (seekBar.value / 100) * audioPlayer.duration;
         });
     });
+});
