c7e0e987cb404eae61d5e83980163f4db91b60d5
Igor Leskov <151756679+igor-leskov@users.noreply.github.com>
2024-04-18 12:02:30 +0300

    Create language.js

---
+++ images/language.js
@@ -0,0 +1,25 @@
+ function redirectToHome() {
+            window.location.href = "index.html";
+        }
+
+        // Функция для определения языка браузера
+        function getBrowserLanguage() {
+            return navigator.language || navigator.userLanguage;
+        }
+
+        // Определяем язык браузера и перенаправляем на соответствующую страницу 404
+        const browserLanguage = getBrowserLanguage();
+        let redirectPage;
+
+        if (browserLanguage.startsWith('et')) {
+            redirectPage = "404_et.html";
+            errorMessage.innerText = "Lehekülge ei leitud";
+        } else {
+            redirectPage = "404.html";
+            errorMessage.innerText = "Страница не найдена";
+        }
+
+        // Перенаправление на выбранную страницу
+        setTimeout(function() {
+            window.location.href = redirectPage;
+        }, 3000); // 3 секунды задержки перед перенаправлением
