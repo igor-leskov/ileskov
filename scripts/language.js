2fd8e71ae0ab688a6e673ef408d11f4600253ba7
Igor Leskov <151756679+igor-leskov@users.noreply.github.com>
2024-04-18 12:17:38 +0300

    Update language.js

--- scripts/language.js
+++ scripts/language.js
@@ -2,22 +2,27 @@ function redirectToHome() {
             window.location.href = "index.html";
         }
 
+        // Функция для определения языка браузера
         function getBrowserLanguage() {
             return navigator.language || navigator.userLanguage;
         }
 
-        const browserLanguage = getBrowserLanguage();
-        let redirectPage;
-        const errorMessage = document.getElementById('errorMessage'); 
+        // Определяем язык браузера и перенаправляем на соответствующую страницу 404
+        document.addEventListener("DOMContentLoaded", function() {
+            const browserLanguage = getBrowserLanguage();
+            let redirectPage;
+            const errorMessage = document.getElementById('errorMessage'); // Объявляем переменную здесь
 
-        if (browserLanguage.startsWith('et')) {
-            redirectPage = "404_et.html";
-            errorMessage.innerText = "Lehekülge ei leitud";
-        } else {
-            redirectPage = "404.html";
-            errorMessage.innerText = "Страница не найдена";
-        }
+            if (browserLanguage.startsWith('et')) {
+                redirectPage = "404_et.html";
+                errorMessage.innerText = "Lehekülge ei leitud";
+            } else {
+                redirectPage = "404.html";
+                errorMessage.innerText = "Страница не найдена";
+            }
 
-        setTimeout(function() {
-            window.location.href = redirectPage;
-        }, 3000); 
+            // Перенаправление на выбранную страницу
+            setTimeout(function() {
+                window.location.href = redirectPage;
+            }, 3000); // 3 секунды задержки перед перенаправлением
+        });
