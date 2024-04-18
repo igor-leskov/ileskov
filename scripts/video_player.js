c90333a42233200d8ede9413dd9ed7296c55953c
Igor Leskov <151756679+igor-leskov@users.noreply.github.com>
2024-04-18 08:18:43 +0300

    Update video_player.js

--- scripts/video_player.js
+++ scripts/video_player.js
@@ -2,15 +2,33 @@ document.addEventListener('DOMContentLoaded', function() {
     var videos = document.querySelectorAll('.video');
 
     videos.forEach(function(video) {
+       
+        video.addEventListener('loadeddata', function() {
+            var savedTime = localStorage.getItem(video.src);
+            if (savedTime) {
+                video.currentTime = parseFloat(savedTime);
+            }
+        });
+
         video.addEventListener('play', function(event) {
             var currentVideo = event.target;
 
+            // Пауза другим видео
             videos.forEach(function(v) {
                 if (v !== currentVideo) {
                     v.pause();
                 }
             });
         });
+
+        video.addEventListener('pause', function() {
+           
+            localStorage.setItem(video.src, video.currentTime);
+        });
+
+        video.addEventListener('ended', function() {
+           
+            localStorage.removeItem(video.src);
+        });
     });
 });
-
