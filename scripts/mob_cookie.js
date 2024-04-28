2227a4792a9408c7bf8421d927ad7352aeb1c537
Igor Leskov <leskov.studio.official@gmail.com>
2024-04-28 11:25:49 +0300

    Update mob_cookie.js

--- scripts/mob_cookie.js
+++ scripts/mob_cookie.js
@@ -55,3 +55,50 @@ window.addEventListener('load', function() {
 
     fixCookieConsentBarPosition();
 });
+
+function hasLiked(newsId) {
+    return getCookie("like_" + newsId) === "true";
+}
+
+function like(newsId) {
+    var likesCountElement = document.querySelector('[data-news-id="' + newsId + '"] .likes-count');
+    var likesCount = parseInt(likesCountElement.innerText);
+    
+    if (!hasLiked(newsId)) {
+        likesCount++;
+        likesCountElement.innerText = likesCount;
+        setCookie("like_" + newsId, "true", 365); 
+    } else {
+        alert("Вы уже ставили отметку Нравится для этой новости!");
+    }
+}
+
+var likeButtons = document.querySelectorAll('.like-button');
+likeButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        var newsId = button.getAttribute('data-news-id');
+        if (getCookie("cookieconsent") === "accepted") {
+            like(newsId);
+        } else {
+            alert("Пожалуйста, согласитесь на использование Cookie, чтобы ставить отметки Нравится новостям.");
+        }
+    });
+});
+
+window.onload = function() {
+    var likesFromCookies = document.cookie.split(';').filter(function(cookie) {
+        return cookie.trim().startsWith("like_");
+    }).reduce(function(acc, cookie) {
+        var newsId = cookie.trim().split('=')[0].split('_')[1];
+        acc[newsId] = (acc[newsId] || 0) + 1;
+        return acc;
+    }, {});
+
+    for (var newsId in likesFromCookies) {
+        var likesCountElement = document.querySelector('[data-news-id="' + newsId + '"] .likes-count');
+        var likesCount = parseInt(likesCountElement.innerText);
+        likesCount += likesFromCookies[newsId];
+        likesCountElement.innerText = likesCount;
+    }
+}
+
