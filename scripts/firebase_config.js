5497649d00e62a2eea12f491264a5646ee228db3
Igor Leskov <leskov.studio.official@gmail.com>
2024-04-30 09:40:00 +0300

    Create firebase_config.js

---
+++ scripts/firebase_config.js
@@ -0,0 +1,19 @@
+<script type="module">
+		  
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCKwIVhZrfDUZB6rQGNFtxtF4pQ3_WCCBk",
+    authDomain: "ileskov-2024.firebaseapp.com",
+    databaseURL: "https://ileskov-2024-default-rtdb.firebaseio.com",
+    projectId: "ileskov-2024",
+    storageBucket: "ileskov-2024.appspot.com",
+    messagingSenderId: "667242483504",
+    appId: "1:667242483504:web:de78b9ef17a5ca7c5402f5",
+    measurementId: "G-FZW0QLDKJY"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
