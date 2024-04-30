a99b61c4ab83d00f376bfb232205aca4bebbbc3c
Igor Leskov <leskov.studio.official@gmail.com>
2024-04-30 12:27:55 +0300

    Update cookie.js

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -1,16 +1,29 @@
-function getLocalStorageItem(name) {
-    return localStorage.getItem(name);
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
 }
 
-function setLocalStorageItem(name, value) {
-    localStorage.setItem(name, value);
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
 function acceptCookies() {
-    setLocalStorageItem("cookieconsent", "accepted");
+    setCookie("cookieconsent", "accepted", 30);
     var consentBar = document.getElementById("cookie-consent-bar");
     if (consentBar) {
-        consentBar.style.display = "none";
+        consentBar.style.display = "none"; 
     }
 }
 
@@ -27,14 +40,17 @@ function redirectToPrivacyPolicyRu() {
 }
 
 function redirectToPrivacyPolicyEt() {
-    var policyPage = "privacy_policy_et.html";
+    var policyPage = "privacy_policy_et.html"; 
     window.location.href = policyPage;
 }
 
-window.addEventListener('DOMContentLoaded', function() {
-    var consentBar = document.getElementById("cookie-consent-bar");
-    if (consentBar && !getLocalStorageItem("cookieconsent")) {
-        consentBar.style.display = "block";
+window.addEventListener('load', function() {
+    
+    if (!getCookie("cookieconsent")) {
+        var consentBar = document.getElementById("cookie-consent-bar");
+        if (consentBar) {
+            consentBar.style.display = "block";
+        }
     }
 
     fixCookieConsentBarPosition();
