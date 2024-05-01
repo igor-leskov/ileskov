8aa9e879eda83d8dfe78fcfd30d996bd7ebfe682
Igor Leskov <leskov.studio.official@gmail.com>
2024-05-01 11:32:40 +0300

    Update modal_gallery.js

--- scripts/modal_gallery.js
+++ scripts/modal_gallery.js
@@ -1,65 +1,57 @@
-var images = [
-    "images/image1.webp",
-    "images/image2.webp",
-    "images/image3.webp",
-    "images/image4.webp",
-    "images/image5.webp",
-    "images/image7.webp",
-    "images/image8.webp",
-    "images/image9.webp",
-    "images/image6.webp"
-];
-var currentImageIndex = 0;
-var currentAudioPlayer = null;
+document.addEventListener("DOMContentLoaded", function() {
+    var images = ["images/image1.webp", "images/image2.webp", "images/image3.webp", "images/image4.webp", "images/image5.webp", "images/image7.webp", "images/image8.webp", "images/image9.webp", "images/image6.webp"];
+    var currentImageIndex = 0;
+    var currentAudioPlayer = null;
 
-function openModal(imageSrc) {
+    function openModal(imageSrc) {
     var modal = document.getElementById("myModal");
     var modalImg = document.getElementById("modalImg");
     modal.style.display = "flex";
     modalImg.src = imageSrc;
     document.body.style.overflow = "hidden";
-    document.body.classList.add("modal-open");
     var scrollTopButton = document.getElementById("scrollTopButton");
-    scrollTopButton.style.display = "none";
+    if (scrollTopButton) {
+        scrollTopButton.style.display = "none";
+    }
+    document.body.classList.add("modal-open");
 }
-
-function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
-    document.body.style.overflow = "auto";
-    document.body.classList.remove("modal-open");
-    document.getElementById("scrollTopButton").focus();
-}
-
-function prevImage() {
-    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-    updateModalImage();
-}
-
-function nextImage() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    updateModalImage();
-}
-
-function updateModalImage() {
-    var modalImg = document.getElementById("modalImg");
-    modalImg.src = images[currentImageIndex];
-}
-
-document.getElementById("modalImg").addEventListener("click", function() {
-    nextImage();
-});
-
-function closeModalOnClick(event) {
-    var modal = document.getElementById("myModal");
-    if (event.target === modal) {
+    
+    function closeModal() {
+        var modal = document.getElementById("myModal");
         modal.style.display = "none";
         document.body.style.overflow = "auto";
         document.body.classList.remove("modal-open");
-        
-        scrollTopButton.style.display = "block";
-        
+        document.getElementById("scrollTopButton").focus();
     }
-}
 
-document.body.addEventListener("click", closeModalOnClick);
+    function prevImage() {
+        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+        updateModalImage();
+    }
+
+    function nextImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        updateModalImage();
+    }
+
+    function updateModalImage() {
+        var modalImg = document.getElementById("modalImg");
+        modalImg.src = images[currentImageIndex];
+    }
+
+    document.getElementById("modalImg").addEventListener("click", function() {
+        nextImage();
+    });
+
+    function closeModalOnClick(event) {
+        var modal = document.getElementById("myModal");
+        if (event.target === modal) {
+            modal.style.display = "none";
+            document.body.style.overflow = "auto";
+            document.body.classList.remove("modal-open");
+            scrollTopButton.style.display = "block";
+        }
+    }
+
+    document.body.addEventListener("click", closeModalOnClick);
+});
