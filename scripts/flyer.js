28ea8d598421a998c065f7442c0b4cf88c2a5d24
Igor Leskov <leskov.studio.official@gmail.com>
2024-05-01 20:38:18 +0300

    Update flyer.js

--- scripts/flyer.js
+++ scripts/flyer.js
@@ -2,19 +2,24 @@ const flyer = document.querySelector('.flyer-container');
 flyer.style.opacity = '0';
 flyer.style.visibility = 'hidden';
 
+let isFlyerVisible = false;
 let hideTimer = null;
 
 function handleScroll() {
     clearTimeout(hideTimer);
 
-    flyer.style.opacity = '1';
-    flyer.style.visibility = 'visible';
+    if (!isFlyerVisible) {
+        flyer.style.opacity = '1';
+        flyer.style.visibility = 'visible';
+        isFlyerVisible = true;
+    }
 
     hideTimer = setTimeout(() => {
         flyer.style.opacity = '0';
      
         setTimeout(() => {
             flyer.style.visibility = 'hidden';
+            isFlyerVisible = false;
         }, 100); 
     }, 1000);
 }
