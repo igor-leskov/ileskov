33413d3ecb980fa1a6b1ae8370726d71868a7b24
Igor Leskov <leskov.studio.official@gmail.com>
2024-05-02 09:29:04 +0300

    Update video_player.js

--- scripts/video_player.js
+++ scripts/video_player.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     var videos = document.querySelectorAll('.video');
+    var audios = document.querySelectorAll('.audio');
 
     videos.forEach(function(video) {
         
         video.addEventListener('loadeddata', function() {
-            var videoKey = video.dataset.key; 
-            var savedTime = localStorage.getItem(videoKey); 
+            var videoKey = video.dataset.key;
+            var savedTime = localStorage.getItem(videoKey);
             if (savedTime) {
                 video.currentTime = parseFloat(savedTime);
             }
@@ -14,23 +15,38 @@ document.addEventListener('DOMContentLoaded', function() {
         video.addEventListener('play', function(event) {
             var currentVideo = event.target;
 
+            
             videos.forEach(function(v) {
                 if (v !== currentVideo) {
                     v.pause();
                 }
             });
+
+          
+            audios.forEach(function(audio) {
+                audio.pause();
+            });
         });
 
         video.addEventListener('pause', function() {
-            
-            var videoKey = video.dataset.key; 
-            localStorage.setItem(videoKey, video.currentTime); 
+            var videoKey = video.dataset.key;
+            localStorage.setItem(videoKey, video.currentTime);
         });
 
         video.addEventListener('ended', function() {
-            
             var videoKey = video.dataset.key;
             localStorage.removeItem(videoKey);
         });
     });
+
+   
+    audios.forEach(function(audio) {
+        audio.addEventListener('play', function() {
+           
+            videos.forEach(function(video) {
+                video.pause();
+            });
+        });
+    });
 });
+
