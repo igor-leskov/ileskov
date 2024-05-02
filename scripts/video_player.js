80f464154e35e4b8dd250f794bf6a7409100b4eb
Igor Leskov <leskov.studio.official@gmail.com>
2024-05-02 09:58:02 +0300

    Update video_player.js

--- scripts/video_player.js
+++ scripts/video_player.js
@@ -1,55 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
     var videos = document.querySelectorAll('.video');
-    var playButtons = document.querySelectorAll('.play-button');
-    var activeAudio = null; 
 
     videos.forEach(function(video) {
-        video.addEventListener('play', function() {
-            if (activeAudio) {
-                activeAudio.pause();
+        
+        video.addEventListener('loadeddata', function() {
+            var videoKey = video.dataset.key; 
+            var savedTime = localStorage.getItem(videoKey); 
+            if (savedTime) {
+                video.currentTime = parseFloat(savedTime);
             }
+        });
+
+        video.addEventListener('play', function(event) {
+            var currentVideo = event.target;
+
             videos.forEach(function(v) {
-                if (v !== video) {
+                if (v !== currentVideo) {
                     v.pause();
                 }
             });
         });
-    });
 
-    playButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var audioSrc = button.getAttribute('data-src');
+        video.addEventListener('pause', function() {
+            
+            var videoKey = video.dataset.key; 
+            localStorage.setItem(videoKey, video.currentTime); 
+        });
 
-            if (!button.audio) {
-                button.audio = new Audio(audioSrc);
-                button.audio.addEventListener('ended', function() {
-                    button.classList.remove('playing');
-                    button.innerHTML = '&#9658;';
-                });
-            }
-
-            if (!button.classList.contains('playing')) {
-                if (activeAudio) {
-                    activeAudio.pause();
-                    document.querySelector('.playing').classList.remove('playing');
-                    document.querySelector('.playing').innerHTML = '&#9658;';
-                }
-                button.audio.play();
-                button.classList.add('playing');
-                button.innerHTML = '&#10074;&#10074;';
-                activeAudio = button.audio;
-            } else {
-                button.audio.pause();
-                button.classList.remove('playing');
-                button.innerHTML = '&#9658;';
-                if (activeAudio === button.audio) {
-                    activeAudio = null;
-                }
-            }
-
-            videos.forEach(function(video) {
-                video.pause();
-            });
+        video.addEventListener('ended', function() {
+            
+            var videoKey = video.dataset.key;
+            localStorage.removeItem(videoKey);
         });
     });
 });
